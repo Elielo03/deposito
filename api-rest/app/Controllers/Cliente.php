da9a359a2bd9a4605a911915abecd14821e6f04b
elielrodriguez3 <101885855+elielrodriguez3@users.noreply.github.com>
2022-10-27 01:50:37 -0500

    create cliente Controller

---
+++ api-rest/app/Controllers/Cliente.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controllers;
+use CodeIgniter\RESTful\ResourceController;
+use CodeIgniter\API\ResponseTrait;
+use App\Models\ClienteModel;
+
+class Cliente extends ResourceController{
+    use ResponseTrait;
+
+    public function getAll(){
+        $model=new ClienteModel();
+        $data['clientes'] = $model->orderBy('id', 'DESC')->findAll();
+        return $this->respond($data);
+    }
+}
