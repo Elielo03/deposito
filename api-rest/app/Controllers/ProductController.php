29f697e1389de1f98fbf4388b05a4ae2b9e33b28
Elielo03 <elielorgz03@gmail.com>
2023-11-23 01:36:47 -0600

    products

--- api-rest/app/Controllers/ProductController.php
+++ api-rest/app/Controllers/ProductController.php
@@ -1,36 +1,26 @@
 <?php
+
+namespace App\Controllers;
+use App\Models\ProductsModel;
+use CodeIgniter\RESTful\ResourceController;
+use CodeIgniter\API\ResponseTrait;
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-require APPPATH . 'libraries/REST_Controller.php';
 
-class Api extends REST_Controller {
+
+class Api extends ResourceController {
 
     public function __construct() {
-        parent::__construct();
-        $this->load->model('ProductsModel');
+       
     }
 
     public function getAll() {
-        $productos = $this->Productos_model->getAll();
-        $this->response($productos, REST_Controller::HTTP_OK);
+        $model=new ProductsModel();
+        $data['products'] = $model->orderBy('id', 'DESC')->findAll();
+        return $this->respond($data);
+
+      
     }
 
-    public function productos_post() {
-        $data = $this->post();
-        $id = $this->Productos_model->insertar_producto($data);
-        $this->response(['id' => $id], REST_Controller::HTTP_CREATED);
-    }
-
-    public function productos_put() {
-        $id = $this->put('id');
-        $data = $this->put();
-        $this->Productos_model->actualizar_producto($id, $data);
-        $this->response(['message' => 'Producto actualizado con éxito'], REST_Controller::HTTP_OK);
-    }
-
-    public function productos_delete() {
-        $id = $this->delete('id');
-        $this->Productos_model->eliminar_producto($id);
-        $this->response(['message' => 'Producto eliminado con éxito'], REST_Controller::HTTP_OK);
-    }
+    
 }
--- api-rest/app/Models/ProductsModel.php
+++ api-rest/app/Models/ProductsModel.php
@@ -1,24 +1,10 @@
 <?php
-class ProductsModel extends CI_Model {
+namespace App\Models;
+use CodeIgniter\Model;
+class ProductsModel extends Model {
     
-    public function getAll() {
-        $query = $this->db->get('productos');
-        return $query->result();
-    }
-
-    public function insertar_producto($data) {
-        $this->db->insert('productos', $data);
-        return $this->db->insert_id();
-    }
-
-    public function actualizar_producto($id, $data) {
-        $this->db->where('id', $id);
-        $this->db->update('productos', $data);
-    }
-
-    public function eliminar_producto($id) {
-        $this->db->where('id', $id);
-        $this->db->delete('productos');
-    }
+    protected $table = 'productos';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['nombre', 'descripcion'];
 }
 ?>
